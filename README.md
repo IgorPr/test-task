750eaf3c5f21477d9f357a3882cd4fcbceb3a21c
Ihor Pohorilyi <piv986@gmail.com>
2024-05-29 23:00:07 +0300

    test-task changes

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 ### 1. Override some existing method for any pimcore model with additional logic.
-### 3. Create implementation for override some existing service (pseudo code)
-
-From my point of view, these tasks are very similar.
-So I decided to do them for one class.
-For this purpose, I used the decorator pattern.
 
 https://pimcore.com/docs/platform/2023.2/Pimcore/Extending_Pimcore/Overriding_Models/
 https://github.com/IgorPr/test-task/blob/6feaf1cfd3c83105dac339a4eb46e1230609d5db/src/Model/DataObject/DebugService.php
@@ -22,4 +17,4 @@ For creating a custom model, I used Doctrine ORM
 https://github.com/IgorPr/test-task/blob/6feaf1cfd3c83105dac339a4eb46e1230609d5db/src/Entity
 
 
-#### P.S. I couldn't run 'php bin/console make' perhaps I encountered a bug. That's why I wrote the code using the Doctrine manual.
+### 3. Create implementation for override some existing service (pseudo code)
---
+++ src/Model/Filter/Dao.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Model\Filter;
+
+use Pimcore\Model\Dao\AbstractDao;
+use Pimcore\Model\Exception\NotFoundException;
+
+class Dao extends AbstractDao
+{
+    protected string $tableName = 'filters';
+    protected string $filterColorsTableName = 'filter_colors';
+    protected string $сolorsTableName = 'colors';
+
+    /**
+     * get vote by id
+     *
+     * @throws NotFoundException
+     */
+    public function getById(?int $id = null): void
+    {
+        if ($id !== null)  {
+            $this->model->setId($id);
+        }
+
+        $data = $this->db->fetchAssociative(
+            'SELECT * FROM ' . $this->tableName . '
+            LEFT JOIN filter_colors ON ' . $this->filterColorsTableName . ' . filter_id = ' . $this->tableName . ' . id
+            LEFT JOIN ' . $this->сolorsTableName . ' ON ' . $this->filterColorsTableName . ' . color_id = ' . $this->сolorsTableName . ' . id
+            WHERE id = ?', [$this->model->getId()]
+        );
+
+        if (!$data) {
+            throw new NotFoundException("Object with the ID " . $this->model->getId() . " doesn't exists");
+        }
+
+        $this->assignVariablesToModel($data);
+
+        $colors = [];
+
+        foreach ($data as $row) {
+            if ($row['color_id']) {
+                $colors[] = ['id' => $row['color_id'], 'name' => $row['color_name']];
+            }
+        }
+
+        $this->model->setColors($colors);
+    }
+
+    public function save(): void
+    {
+        $vars = get_object_vars($this->model);
+
+        $buffer = [];
+
+        $validColumns = $this->getValidTableColumns($this->tableName);
+
+        if (count($vars)) {
+            foreach ($vars as $k => $v) {
+                if (!in_array($k, $validColumns)) {
+                    continue;
+                }
+
+                $getter = "get" . ucfirst($k);
+
+                if (!is_callable([$this->model, $getter])) {
+                    continue;
+                }
+
+                $value = $this->model->$getter();
+
+                if (is_bool($value)) {
+                    $value = (int)$value;
+                }
+
+                $buffer[$k] = $value;
+            }
+        }
+
+        if ($this->model->getId() !== null) {
+            $this->db->update($this->tableName, $buffer, ["id" => $this->model->getId()]);
+        } else {
+            $this->db->insert($this->tableName, $buffer);
+            $this->model->setId($this->db->lastInsertId());
+        }
+
+        if ($this->model->getId() !== null) {
+            $this->db->delete('filter_colors', ['filter_id' => $this->model->getId()]);
+
+            foreach ($this->model->getColors() as $color) {
+                $this->db->insert($this->filterColorsTableName, [
+                    'filter_id' => $this->model->getId(),
+                    'color_id' => $color['id'],
+                ]);
+            }
+        }
+    }
+
+    public function delete(): void
+    {
+        $this->db->delete($this->tableName, ["id" => $this->model->getId()]);
+        $this->db->delete($this->filterColorsTableName, ["filter_id" => $this->model->getId()]);
+    }
+}
